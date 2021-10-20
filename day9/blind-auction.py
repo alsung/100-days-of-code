e2a3e111e26b76c6dfef76aeaac10cce362f33a7
Alex Sung <alexsung98@gmail.com>
2021-10-19 19:40:20 -0700

    day 9 project completed blind auction with logo

--- day9/blind-auction.py
+++ day9/blind-auction.py
@@ -1,1 +1,43 @@
-# Day 9.3
+# Day 9.3 Blind Auction
+
+import os
+
+def clear():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):
+        command = 'cls'
+    os.system(command)
+
+# print logo
+import art
+print(art.logo)
+
+bid_dict = {}
+other_bids = "yes"
+
+while other_bids == "yes":
+    # ask for Name input
+    name = input("What is your name?\n")
+
+    # ask for bid price
+    bid_price = int(input("What is your bid price? $"))
+
+    # add name and bid price to dictionary
+    bid_dict[name] = bid_price
+
+    # ask if there are other uses who want to bid
+    other_bids = input("Are there other bidders? Type 'yes' or 'no'.\n")
+
+    
+    
+    clear()
+    
+if other_bids == "no":
+    highest_bid = 0
+    highest_bidder = ""
+    for name in bid_dict:
+        if bid_dict[name] > highest_bid:
+            highest_bid = bid_dict[name]
+            highest_bidder = name
+    
+    print(f"The winner is {highest_bidder} with ${highest_bid}.")
