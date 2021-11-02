1ff604fc68559b88a8b21f50dc91649ff7a23f56
Alex Sung <alexsung98@gmail.com>
2021-11-01 19:40:34 -0700

    day 16 coffee machine oop project completed

--- day16/CoffeeMachine/main.py
+++ day16/CoffeeMachine/main.py
@@ -4,17 +4,31 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+
+# TODO 2: Check resources sufficient?
+    # coffee_maker.py
+# TODO 3: Process coins
+# TODO 4: Check transation successful?
+# TODO 5: Make coffee.
+
 is_on = True
-coffee_machine = CoffeeMaker()
-money_maker = MoneyMachine()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
 
 while is_on:
-
-    choice = input(" What would you like? (espresso/latte/cappuccino): ")
+    options = menu.get_items()
+    choice = input(f" What would you like? ({options}): ")
 
     if choice == "off":
         print("Goodbye!")
         is_on = False
     elif choice == "report":
-        coffee_machine.report()
-        money_maker.report()
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
