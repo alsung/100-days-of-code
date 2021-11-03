ac0e8af072f3d39b493b4a84bc0b3e25719f03a5
Alex Sung <alexsung98@gmail.com>
2021-11-02 20:13:56 -0700

    day 17 quiz project completed

--- day17/quiz_brain.py
+++ day17/quiz_brain.py
@@ -1,21 +1,29 @@
-# TODO: asking the questions
-# TODO: checking if the answer was correct
-# TODO: checking if we're at the end of the quiz
-
 class QuizBrain:
 
     def __init__(self, q_bank):
         self.question_number = 0
         self.question_list = q_bank
+        self.score = 0
+
 
     def next_question(self):
         """Retrieve an item from current question_number from the question_list.
         Shows the user the Question text and ask for the user's answer."""
         question = self.question_list[self.question_number]
         self.question_number += 1
-        answer = input(f"Q.{self.question_number}: {question.text} (True/False)?: ")
-        print(answer)
+        user_answer = input(f"Q.{self.question_number}: {question.text} (True/False)?: ")
+        self.check_answer(user_answer, question.answer)
 
     def still_has_questions(self):
         """Return True if there are still questions to be asked, False otherwise."""
         return self.question_number < len(self.question_list)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
