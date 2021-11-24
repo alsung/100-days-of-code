2d9e966a53dbdbff138aee076c33288f86b0cc3a
Alex Sung <alexsung98@gmail.com>
2021-11-23 17:40:25 -0800

    day 20 & 21 completed snake game
    class-inheritance.py: examples of how inheritance is used in Python
    food.py: food class for snake game
    main.py: main file to execute when running snake game
    scoreboard.py: scoreboard class and methods for snake game
    snake.py: snake class

--- day20-21/snake_game/snake.py
+++ day20-21/snake_game/snake.py
@@ -16,11 +16,19 @@ class Snake:
     def create_snake(self):
         """Creates a 3 segment snake at provided starting positions"""
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+            
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        """Add a new segment to the snake."""
+        self.add_segment(self.segments[-1].position())
 
 
     # get hold of last segment first, then move segment to position of 2nd to 
