f52f5985acf0308669743dce61c27467ed08e53a
Alex Sung <alexsung98@gmail.com>
2021-12-16 16:13:27 -0800

    day 27 update, pack(), place(), and grid(); adding padding to components

--- day27/main.py
+++ day27/main.py
@@ -10,26 +10,35 @@ def button_clicked():
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 
 #Label
 
 my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
 #Update properties of a component we created
 my_label.config(text="New Text")
-my_label.pack() # place component into window at center of screen
-
+# my_label.pack() # place component into window at center of screen from top to bottom
+# my_label.place(x=100, y=200) # place widget at precise coordinate
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
 
 #Button
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+# button.pack()
+button.grid(column=1, row=1)
+
+# New Button
+new_button = Button(text="Click Me", command=button_clicked)
+new_button.grid(column=2, row=0)
+
 
 # Entry (Input)
-
 input = Entry(width=10)
 print(input.get())
-input.pack()
+# input.pack()
+input.grid(column=3, row=2)
 
 
-
+ 
 
 window.mainloop()
